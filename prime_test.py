3b14a034544687a2136faa491497dc67df6dd210
Odur Joseph <odurjoseph8@gmail.com>
2018-04-25 13:26:47 +0300

    added test case

--- prime_test.py
+++ prime_test.py
@@ -6,6 +6,9 @@ class prime_tests(unittest.TestCase):
     def test_return_type(self):
         self.assertEqual(type(prime_generator()), list, msg='The returned type does not match the type list' )
 
+    def test_default(self):
+        self.assertEqual(prime_generator(), [2])
+
     def test_with_start_value_zero(self):
         self.assertEqual(prime_generator(end_value=20), [2, 3, 5, 7, 11, 13, 17, 19], msg='The expected output differs')
         self.assertEqual(prime_generator(end_value=27), [2, 3, 5, 7, 11, 13, 17, 19, 23], msg='The expected output differs')
