bc551284e07352b61b96dcb863353b48c3e406da
Odur Joseph <odurjoseph8@gmail.com>
2018-04-25 12:22:43 +0300

    wrote code for first test

---
+++ prime_generator.py
@@ -0,0 +1,3 @@
+
+def prime_generator(start_value=0, end_value=1):
+    return []
--- prime_test.py
+++ prime_test.py
@@ -1,15 +1,18 @@
 import unittest
 from .prime_generator import prime_generator
 
-class prime_tests(unittest.TestCase):
 
+class prime_tests(unittest.TestCase):
     def test_return_type(self):
-        self.assertEqual(type(prime_generator()), list)
+        self.assertEqual(type(prime_generator()), list, msg='The returned type does not match the type list' )
 
     def test_with_start_value_zero(self):
-        self.assertEqual(prime_generator(endvalue=20), [2, 3, 5, 7, 11, 13, 17, 19])
-        self.assertEqual(prime_generator(endvalue=27), [2, 3, 5, 7, 11, 13, 17, 19, 23])
+        self.assertEqual(prime_generator(end_value=20), [2, 3, 5, 7, 11, 13, 17, 19], msg='The expected output differs')
+        self.assertEqual(prime_generator(end_value=27), [2, 3, 5, 7, 11, 13, 17, 19, 23], msg='The expected output differs')
 
     def test_with_start_and_end_values_set(self):
-        self.assertEquals()
+        self.assertEqual(prime_generator(start_value=3, end_value=18), [3, 5, 7, 11, 13, 17], msg='The expected output differs')
 
+
+if __name__ == '__main__':
+    unittest.main()
